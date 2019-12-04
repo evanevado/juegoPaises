e88d88be8572557c68a3a25b843296ee4dd48af7
eva <enevado@csi.uned.es>
2019-12-04 13:36:57 +0100

    dummy

--- LibreriasJuego/src/LibreriasJuego/Dummy.cs
+++ LibreriasJuego/src/LibreriasJuego/Dummy.cs
@@ -8,7 +8,21 @@ namespace LibreriasJuego.src.LibreriasJuego
     {
         public static void Main()
         {
+            pintarEvaluacion(cuadrado, 5);
+            //equivale a
+            pintarEvaluacion((j)=>j*j,5);
+            //le pongo j como cualquier nombre... estoy diciendo que recibe un parámetro y lo que tiene que hacer con él
+        }
 
+        public static int cuadrado(int x)
+        {
+            return x * x;
+        }
+
+        public static void pintarEvaluacion(Func<int, int> funcion, int x)
+        {
+            int resultado = funcion(x);
+            Console.WriteLine("El resultado es " + resultado);
         }
     }
 }
